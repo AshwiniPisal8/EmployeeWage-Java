76fd29eb3c2cadb8358502456124f1910c943096
Ashwini <ashwinipatil@gmail.com>
2023-04-25 00:30:06 +0530

    [ADDED]:[AShwini] Employee Wage Project setup with Initial Position

---
+++ src/com/infotech/employeewage/EmployeeWage.java
@@ -0,0 +1,25 @@
+package com.infotech.employeewage;
+
+import java.util.Random;
+
+public class EmployeeWage {
+
+	public static void main(String[] args) {
+		/** Welcome massage for user**/
+		System.out.println("Welcome to Employee Wage Computation Program");
+		int attendance;
+		final int IS_ABSENT=0;
+		
+		Random random = new Random();
+		attendance = random.nextInt(2);
+		System.out.println("Attendance ="+attendance);
+		
+		if(attendance == IS_ABSENT) {
+			System.out.println("Employee is Absent");
+		}else {
+			System.out.println("Employee is Present ");
+		}
+
+	}
+
+}
