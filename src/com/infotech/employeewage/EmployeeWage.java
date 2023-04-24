65feb7530f4a8a31f30fada9a47f1fd2efa33a78
Ashwini <ashwinipatil@gmail.com>
2023-04-25 01:09:05 +0530

    [MODIFIED]:[Ashwini] Check For Employee Part Time or Full Time

--- src/com/infotech/employeewage/EmployeeWage.java
+++ src/com/infotech/employeewage/EmployeeWage.java
@@ -7,12 +7,16 @@ public class EmployeeWage {
 	public static void main(String[] args) {
 		/** Welcome massage for user**/
 		System.out.println("Welcome to Employee Wage Computation Program");
+		
 		int attendance;
 		int daily_wage = 0;
+		int empType;
 		
 		final int IS_ABSENT = 0;
 		final int WAGE_PER_HR = 20;
-		final int FULL_DAY_HR = 8;
+		final int FULL_TIME_HR = 8;
+		final int PART_TIME_HR = 4;
+		final int IS_PART_TIME = 0;
 		
 		Random random = new Random();
 		attendance = random.nextInt(2);
@@ -20,11 +24,22 @@ public class EmployeeWage {
 		
 		if(attendance == IS_ABSENT) {
 			System.out.println("Employee is Absent");
-		}else {
-			System.out.println("Employee is Present ");
-			daily_wage =WAGE_PER_HR * FULL_DAY_HR;
 		}
+           else {
+			System.out.println("Employee is Present ");
+			empType = random.nextInt(2);
+			if(empType == IS_PART_TIME) {
+				System.out.println("Employee is PartTime");
+				daily_wage = WAGE_PER_HR * PART_TIME_HR;
+			}
+				else {
+					System.out.println("Employee is FullTime");
+					daily_wage = WAGE_PER_HR * FULL_TIME_HR;
+				}
+				
+			}
+		
           System.out.println("Daily Wage ="+daily_wage);
 	}
-
 }
+
