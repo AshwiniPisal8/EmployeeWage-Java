002f2c031c7c050429ddc4ee28c52c366e451e87
Ashwini <ashwinipatil@gmail.com>
2023-04-25 00:43:50 +0530

    [MODIFIED]:[Ashwini] Calculate Daily Wages For Employee

--- src/com/infotech/employeewage/EmployeeWage.java
+++ src/com/infotech/employeewage/EmployeeWage.java
@@ -8,7 +8,11 @@ public class EmployeeWage {
 		/** Welcome massage for user**/
 		System.out.println("Welcome to Employee Wage Computation Program");
 		int attendance;
-		final int IS_ABSENT=0;
+		int daily_wage = 0;
+		
+		final int IS_ABSENT = 0;
+		final int WAGE_PER_HR = 20;
+		final int FULL_DAY_HR = 8;
 		
 		Random random = new Random();
 		attendance = random.nextInt(2);
@@ -18,8 +22,9 @@ public class EmployeeWage {
 			System.out.println("Employee is Absent");
 		}else {
 			System.out.println("Employee is Present ");
+			daily_wage =WAGE_PER_HR * FULL_DAY_HR;
 		}
-
+          System.out.println("Daily Wage ="+daily_wage);
 	}
 
 }
